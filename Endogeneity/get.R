d8c88869dfad4c4aa780dbe5be315c7d66164b8b
tsukasakasa1231 <tsukasakasa1231@gmail.com>
2017-11-16 10:42:55 +0800

    Add files via upload

---
+++ Endogeneity/get.R
@@ -0,0 +1,33 @@
+library(jsonlite)
+library(httr)
+options(stringsAsFactors = FALSE)
+
+# 1. Get and convert to an R object ---------------------------------------
+Date <- Sys.time()
+while(TRUE)
+{
+  if(as.numeric(format(Sys.time(), "%M"))!=as.numeric(format(Date, "%M"))&as.numeric(format(Sys.time(), "%M"))%%5==0)
+  {
+    Date <- Sys.time()
+    url <- "http://data.taipei/youbike"
+    ubike.list <- fromJSON(content(GET(url),"text"))
+
+    # 2. list -> vector -> matrix -> data.frame -------------------------------
+    
+    # Select the right node and unlist it --> vector 只選擇"return value"
+    ubike.v <- unlist(ubike.list$retVal)
+    
+    # Fold it by a specified width --> matrix
+    ubike.m <- matrix(ubike.v, byrow = T, ncol = 14)
+    
+    
+    # Convert the matrix to dataframe
+    ubike.df <- as.data.frame(ubike.m)
+    ubike.df["Time"]<-Date
+    
+    
+    write.csv(ubike.df, file=paste(Date, ".csv", sep=""))
+  }
+  
+}
+
